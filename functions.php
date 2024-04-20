15149d03b2b25c7e05920f93aef3ec43922467ab
anitarozsa <anitarozsa11@gmail.com>
2024-04-20 14:13:48 +0200

    new files for functions, DELETE PROFIL button in profile

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+  function save_users(string $path, array $data) {
+    $users = load_users($path);
+
+    $users["users"][] = $data;
+
+    $json_data = json_encode($users, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+
+    file_put_contents($path, $json_data);
+  }
+
+  function load_users(string $path): array {
+    if (!file_exists($path))
+      die("Nem sikerült a fájl megnyitása!");
+
+    $json = file_get_contents($path);
+
+    return json_decode($json, true);
+  }
+?>
